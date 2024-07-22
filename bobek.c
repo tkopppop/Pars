24b48c2b96f7d4e66ab722597aa21846e94efee9
Genius Hacker tf8abcabc (ebook author as THE professional author for cybersecurity) <156498741+tkopppop@users.noreply.github.com>
2024-07-22 11:30:24 +0900

    Update bobek.c

--- bobek.c
+++ bobek.c
@@ -1,9 +1,1 @@
-venglin was a elite
-i use the title author AKA for it
-sorry. i agreed with your codes
-and i think 'this boy was to be a awesome guy and 1337'
-i was a hacker also having same mistakes in now and future thinking
-but i made easteregg comment in it bobek.c and other research paper also similar with this
-thanks venglin the country is nice?!
-        thanks
-        
+-
